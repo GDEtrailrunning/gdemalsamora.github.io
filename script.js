07d52289c4c2d30cf366a70e3fec2b79ba344bd5
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2026-08-23 19:36:15 -0300

    Subo semana 32 y 33

--- script.js
+++ script.js
@@ -98,8 +98,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const dataLine1 = {
     labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
     datasets: [{
-      label: 'Km recorridos en Semana 30',
-      data: [10, 15, 12, 24], // Datos de los km recorridos en cada día
+      label: 'Km recorridos en Semana 32',
+      data: [16, 13, 13, 26], // Datos de los km recorridos en cada día
       borderColor: 'rgba(45, 74, 62, 1)',
       backgroundColor: 'rgba(45, 74, 62, 0.16)',
       fill: true
@@ -109,8 +109,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const dataLine2 = {
     labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
     datasets: [{
-      label: 'Total Km recorridos en Semana 31',
-      data: [14, 14, 12, 19],// Datos de los km recorridos en cada día
+      label: 'Total Km recorridos en Semana 33',
+      data: [14, 14, 14, 30],// Datos de los km recorridos en cada día
       borderColor: 'rgba(45, 78, 199, 1)',
       backgroundColor: 'rgba(45, 78, 199, 0.14)',
       fill: true
@@ -134,7 +134,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function actualizarDiasRestantes() {
   // Fecha objetivo fija
-  const objetivoDate = new Date('2026-08-17');
+  const objetivoDate = new Date('2026-09-27');
   const currentDate = new Date();
   const timeDiff = objetivoDate - currentDate;
   const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
